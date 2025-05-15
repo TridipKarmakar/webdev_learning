548171115222f4eb0461ebc41635d2c26f38a62b
Tridip Karmakar <karmakartridip97@gmail.com>
2025-05-15 22:27:36 +0530

    understanding the DOM

---
+++ JS/DOM/sript.js
@@ -0,0 +1,7 @@
+//example 1
+document.getElementById("changeTextButton").addEventListener("click", () => {
+  let paragraph = document.getElementById("myParagraph");
+  paragraph.textContent = "The paragraph is now changes";
+});
+
+// example 2
