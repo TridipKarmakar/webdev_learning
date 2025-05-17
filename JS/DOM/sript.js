8a18ff16c21f2c6e17d5c32f3ef0b58370189809
Tridip Karmakar <tridipkarmakar97@gmail.com>
2025-05-17 11:35:43 +0530

    practice the example 3

--- JS/DOM/sript.js
+++ JS/DOM/sript.js
@@ -12,3 +12,12 @@ document
     let cityList = document.getElementById("citiesList");
     console.log(cityList.firstElementChild.classList.add("highlight"));
   });
+
+// example 3
+
+document.getElementById("ChangeOrder").addEventListener("click", () => {
+  let ChangeOrder = document.getElementById("coffeeType");
+  ChangeOrder.textContent = "Espresso";
+  ChangeOrder.classList.add("highlight"); // this the add the style of css style code
+  ChangeOrder.style.backgroundColor = "green";
+});
