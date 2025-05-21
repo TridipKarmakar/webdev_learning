df3b2c15e05001a6fa760a21151c290d28f5c760
Tridip Karmakar <karmakartridip97@gmail.com>
2025-05-22 00:08:28 +0530

    understadn the basic asynchronous topics

---
+++ JS/advanceJS/explore1.js
@@ -0,0 +1,6 @@
+/* Like setTimeout function is not in build function */
+setTimeout(() => {
+  console.log("this is the time out function");
+}, 2000);
+
+console.log("This is individual function");
