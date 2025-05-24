a1c823b9db2ab566923558a9dfe3a1667202132c
Tridip Karmakar <karmakartridip97@gmail.com>
2025-05-24 21:04:40 +0530

    understand the promises

--- JS/advanceJS/adv-js-topics.js
+++ JS/advanceJS/adv-js-topics.js
@@ -9,4 +9,25 @@ function outer() {
 }
 
 let increment = outer();
-console.log(increment());
+// console.log(increment());
+
+// promises
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let success = true;
+      if (success) {
+        resolve("Data fetched successfully");
+      } else {
+        reject("Error fetching data");
+      }
+    }, 3000);
+  });
+}
+
+fetchData()
+  .then((data) => {
+    console.log(data.toLowerCase());
+  })
+  .catch((error) => console.log(error));
