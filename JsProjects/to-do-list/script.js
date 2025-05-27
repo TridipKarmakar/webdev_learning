eea5a6529b02f45d7a6033d6e499afd7e6319d9e
Tridip Karmakar <karmakartridip97@gmail.com>
2025-05-27 21:26:08 +0530

    understand the code part before the delete portion

--- JsProjects/to-do-list/script.js
+++ JsProjects/to-do-list/script.js
@@ -1,1 +1,41 @@
-document.getElementById;
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
+
+  let taskList = JSON.parse(localStorage.getItem("task")) || [];
+
+  taskList.forEach((task) => {
+    renderTask(task);
+  });
+
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+
+    if (taskText === "") return;
+    const task = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    taskList.push(task);
+    saveTask();
+    todoInput.value = "";
+    console.log(taskList);
+  });
+
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>
+    `;
+
+    todoList.appendChild(li);
+  }
+
+  function saveTask() {
+    localStorage.setItem("task", JSON.stringify(taskList));
+  }
+});
