5d0c70f2f5796c7c36e15b6c66da4e6fd810f074
Tridip Karmakar <karmakartridip97@gmail.com>
2025-05-30 23:45:53 +0530

    create task form new

--- JS/advanceJS/this-context.js
+++ JS/advanceJS/this-context.js
@@ -16,3 +16,11 @@ boundGreet();
 
 //bind, call and apply
 const boundGreet2 = person.greet.call({ name: "kholi" });
+
+const arrrayCollection = {
+  id: 1748359865091,
+  text: "Action Log generator",
+  completed: false,
+};
+
+console.log(JSON.stringify(arrrayCollection));
