5a93b50e34baa9c953fc70dd147836696cbfad68
Tridip Karmakar <tridipkarmakar97@gmail.com>
2025-06-03 18:52:46 +0530

    understand the li part

--- src/script.js
+++ src/script.js
@@ -1,7 +1,12 @@
 const todoInput = document.getElementById("todo-input");
 const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
 
 let taskList = [];
+
+taskList.forEach((task) => {
+  renderTask(task);
+});
 
 addTaskButton.addEventListener("click", () => {
   const taskText = todoInput.value.trim();
@@ -13,4 +18,13 @@ addTaskButton.addEventListener("click", () => {
   };
   taskList.push(task);
   console.log(taskList);
+  renderTask(task);
 });
+
+function renderTask(task) {
+  const li = document.createElement("li");
+  //   li.setAttribute("data-id", task.id);
+  //   if (task.completed) li.classList.add("line-through opacity-70 text-gray-700");
+  li.innerHTML = `<span class="bg-[#333333] p-2.5 mb-2.5 flex justify-between items-center rounded-sm">${task.text}</span>`;
+  todoList.appendChild(li);
+}
